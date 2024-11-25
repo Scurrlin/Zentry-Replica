3fa4168696840328a6490f71c08d9b92f95cc446
Sean Currlin <sean.currlin@gmail.com>
2024-11-25 17:40:37 -0500

    update Features.jsx

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -103,7 +103,7 @@ const Features = () => (
     <div className="container mx-auto px-3 md:px-10">
       <div className="px-5 py-32">
         <p className="font-circular-web text-lg text-blue-50">
-          Into the Metagame Layer
+          Dive into the 'Game of Games' Universe
         </p>
         <p className="max-w-md font-circular-web text-lg text-blue-50 opacity-50">
           Immerse yourself in a rich and ever-expanding universe where a vibrant
