90b26e2bcc6a3aac0434a8d32f1f210c3b190220
Sean Currlin <sean.currlin@gmail.com>
2024-11-26 18:24:31 -0500

    mobile ui changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -83,10 +83,10 @@ const Hero = () => {
   const getVideoSrc = (index) => `/videos/hero-${index}.mp4`;
 
   return (
-    <div className="relative h-dvh w-screen overflow-x-hidden">
+    <div className="relative h-screen w-screen overflow-x-hidden">
       {loading && (
-        <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
-          {/* https://uiverse.io/G4b413l/tidy-walrus-92 */}
+        <div className="flex-center absolute z-[100] h-screen w-screen overflow-hidden bg-violet-50">
+          {/* Loading animation */}
           <div className="three-body">
             <div className="three-body__dot"></div>
             <div className="three-body__dot"></div>
@@ -97,10 +97,10 @@ const Hero = () => {
 
       <div
         id="video-frame"
-        className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
+        className="relative z-10 h-screen w-screen overflow-hidden rounded-lg bg-blue-75"
       >
         <div>
-          <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
+          <div className="mask-clip-path absolute-center absolute z-50 w-64 h-64 cursor-pointer overflow-hidden rounded-lg">
             <VideoPreview>
               <div
                 onClick={handleMiniVdClick}
@@ -113,7 +113,7 @@ const Hero = () => {
                   muted
                   playsInline
                   id="current-video"
-                  className="size-64 origin-center scale-150 object-cover object-center"
+                  className="w-64 h-64 origin-center scale-150 object-cover object-center"
                   onLoadedData={handleVideoLoad}
                 />
               </div>
@@ -127,7 +127,7 @@ const Hero = () => {
             muted
             playsInline
             id="next-video"
-            className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
+            className="absolute-center invisible absolute z-20 w-64 h-64 object-cover object-center"
             onLoadedData={handleVideoLoad}
           />
           <video
@@ -138,7 +138,7 @@ const Hero = () => {
             loop
             muted
             playsInline
-            className="absolute left-0 top-0 size-full object-cover object-center"
+            className="absolute left-0 top-0 w-full h-full object-cover object-center"
             onLoadedData={handleVideoLoad}
           />
         </div>
@@ -147,7 +147,7 @@ const Hero = () => {
           G<b>A</b>MING
         </h1>
 
-        <div className="absolute left-0 top-0 z-40 size-full">
+        <div className="absolute left-0 top-0 z-40 w-full h-full">
           <div className="mt-24 px-5 sm:px-10">
             <h1 className="special-font hero-heading text-blue-100">
               redefi<b>n</b>e
