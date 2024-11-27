6ff5a1b7306d825986e7491edc79a02d53dc90ee
Sean Currlin <sean.currlin@gmail.com>
2024-11-27 00:21:50 -0500

    mobile ui changes

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -124,7 +124,7 @@ const Features = () => (
         />
       </BentoTilt>
 
-      <div className="grid h-[135vh] w-full grid-cols-2 grid-rows-3 gap-7">
+      <div className="grid h-[135vh] w-full grid-cols-2 gap-7">
         <BentoTilt className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
           <BentoCard
             src="videos/feature-2.mp4"
@@ -138,7 +138,7 @@ const Features = () => (
           />
         </BentoTilt>
 
-        <BentoTilt className="bento-tilt_1 row-span-1 col-span-2 md:col-span-1 ms-0 md:ms-32">
+        <BentoTilt className="bento-tilt_1 row-span-1 col-span-2 md:col-span-1">
           <BentoCard
             src="videos/feature-3.mp4"
             title={
@@ -151,7 +151,7 @@ const Features = () => (
           />
         </BentoTilt>
 
-        <BentoTilt className="bento-tilt_1 row-span-1 col-span-2 md:col-span-1 me-0 md:me-14">
+        <BentoTilt className="bento-tilt_1 row-span-1 col-span-2 md:col-span-1">
           <BentoCard
             src="videos/feature-4.mp4"
             title={
