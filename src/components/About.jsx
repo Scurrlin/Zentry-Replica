3e207fe81be9cc22ddb2c1794f54db869ebf658d
Sean Currlin <sean.currlin@gmail.com>
2025-02-22 10:31:56 -0500

    update mobile styles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -39,8 +39,11 @@ const About = () => {
         />
 
         <div className="about-subtext">
-          <p>The Game of Games begins-your life, now an epic MMORPG</p>
-          <p>Zentry unites every player from countless games and platforms, both digital and physical, into a unified Play Economy</p>
+          <p>The Game of Games begins—your life, now an epic MMORPG</p>
+          <p className="text-gray-500">
+            Zentry unites every player from countless games and platforms, both
+            digital and physical, into a unified Play Economy
+          </p>
         </div>
       </div>
 
